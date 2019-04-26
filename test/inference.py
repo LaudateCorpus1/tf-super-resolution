bf35fc32be16e804a9ecda64e5d4c9f9c93e0796
Stellata <michaelkenmiyajima@gmail.com>
2019-04-26 12:22:36 -0700

    Add files via upload

--- test/inference.py
+++ test/inference.py
@@ -72,11 +72,11 @@ def initialize_model():
                 #image = tf.read_file(inputs_dict["image"])
                 image = inputs_dict["image"]
                 #print("initial image ",image)
-                image = image*4
+                #image = image*4
                 #print("post *4 ",image)
-                image = tf.io.decode_raw(image,out_type = tf.float32)
-                #image = tf.io.decode_jpeg(image)
-                image/=4#does ssmth idk yet con
+                #image = tf.io.decode_raw(image,out_type = tf.float32)
+                image = tf.io.decode_jpeg(image)
+                #image/=4#does ssmth idk yet con
                 print("post decode ",image)
                 image = tf.reshape(image,[1,-1,1,1])
                 print("post reshape",image)
