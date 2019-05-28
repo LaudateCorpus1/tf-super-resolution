073ff44d87aad6cff795921fa4cab8f5bc82975d
Kevin Baragona <kmbaragona@gmail.com>
2019-05-28 02:08:06 -0700

    simplify Dockerfile and code

--- Dockerfile
+++ Dockerfile
@@ -1,47 +1,17 @@
-FROM nvidia/cuda:9.0-base-ubuntu16.04
+FROM tensorflow/tensorflow:1.12.0-gpu-py3
 
-RUN apt-get update && apt-get install -y --no-install-recommends \
-        python3-pip=8.1.1-2ubuntu0.4 \
-        python3-dev=3.5.1-3 \
-        python3-setuptools=20.7.0-1 \
-        python3-tk=3.5.1-1 \
-        wget \
-        git=1:2.7.4-0ubuntu1.6 
+RUN apt-get update && apt-get install -y --no-install-recommends wget
 
-RUN pip3 install -U pip
-
-RUN pip3 install Pillow
-RUN pip3 install tensorflow-gpu
-RUN pip3 install tensorflow
-RUN pip3 --no-cache-dir install \
-    backports.weakref==1.0rc1 \
-    bleach==1.5.0 \
-    cycler==0.10.0 \
-    decorator==4.1.2 \
-    h5py==2.7.0 \
-    html5lib==0.9999999 \
-    Markdown==2.6.8 \
-    matplotlib==2.0.2 \
-    networkx==1.11 \
-    numpy==1.13.3 \
-    olefile==0.44 \
-    protobuf==3.6.1 \
-    pyparsing==2.2.0 \
-    python-dateutil==2.6.1 \
-    pytz==2017.2 \
-    PyWavelets==0.5.2 \
+RUN pip3 install \
     scikit-image==0.13.0 \
     scipy==0.19.1 \
-    six==1.10.0 \
-    Werkzeug==0.12.2
-    
+    Pillow \
+    ai-integration==1.0.6
+COPY . /tf-perceptual-eusr
 
-RUN pip3 install ai-integration
-
-COPY . /tf-perceptual-eusr/test
-
-WORKDIR /tf-perceptual-eusr/test
+WORKDIR /tf-perceptual-eusr
 
 RUN cd test && wget -nc https://s3-us-west-2.amazonaws.com/deepai-opensource-codebases-models/tf-super-resolution/4pp_eusr_pirm.pb
 
+CMD []
 ENTRYPOINT ["python3", "test/entrypoint.py"]
--- test/inference.py
+++ test/inference.py
@@ -6,6 +6,11 @@ from PIL import Image
 import ai_integration
 
 
+#TODO ensure model loads only once
+#TODO run on GPU
+#TODO no temp files
+
+
 def save_image_in_memory(image):
     image = image.convert('RGB')
     imgByteArr = io.BytesIO()
@@ -20,7 +25,6 @@ def initialize_model():
         config = tf.ConfigProto()
         model_input_path = tf.placeholder(tf.string, [])
         model_output_path = tf.placeholder(tf.string, [])
-        data = tf.placeholder(tf.string,shape=[])
         config.gpu_options.allow_growth = True
         config.gpu_options.per_process_gpu_memory_fraction = 0.12
         sess = tf.Session(config=tf.ConfigProto(
@@ -32,36 +36,35 @@ def initialize_model():
         while True:
             with ai_integration.get_next_input(inputs_schema={
                 "image": {
-                "type": "image"
+                    "type": "image"
                 }
             }) as inputs_dict:
                 print("post sess declare")
                 image = inputs_dict["image"]
-                image = [tf.io.decode_image(image,dtype=tf.uint8,channels = 3)]
+                image = [tf.io.decode_image(image, dtype=tf.uint8, channels=3)]
                 image = tf.cast(image, tf.float32)
                 with tf.gfile.GFile("test/4pp_eusr_pirm.pb", 'rb') as f:
-                    model_graph_def = tf.GraphDef()#example
+                    model_graph_def = tf.GraphDef()  # example
                     model_graph_def.ParseFromString(f.read())
-     
-                model_output = tf.import_graph_def(model_graph_def, name='model', input_map={'sr_input:0': image}, return_elements=['sr_output:0'])[0]
+
+                model_output = tf.import_graph_def(model_graph_def, name='model', input_map={'sr_input:0': image},
+                                                   return_elements=['sr_output:0'])[0]
                 model_output = model_output[0, :, :, :]
                 model_output = tf.round(model_output)
                 model_output = tf.clip_by_value(model_output, 0, 255)
                 model_output = tf.cast(model_output, tf.uint8)
-                image = tf.image.encode_png(model_output)#RIGHT. HERE.
+                image = tf.image.encode_png(model_output)
                 write_op = tf.write_file(model_output_path, image)
                 result_data = {"content-type": 'text/plain',
                                "data": None,
                                "success": False,
                                "error": None}
-                image_path_list = []
-                image_byte_list = []
                 out = 'dummy.png'
                 output_path = os.path.join('SR', out)
                 input_path = os.path.join('LR', 'bleh.png')
                 print('- %s -> %s' % ('', output_path))
-                sess.run([write_op], feed_dict={model_input_path:input_path, model_output_path:output_path})
-                file = Image.open(output_path,'r')
+                sess.run([write_op], feed_dict={model_input_path: input_path, model_output_path: output_path})
+                file = Image.open(output_path, 'r')
                 imgbytes = save_image_in_memory(file)
                 output_img_bytes = imgbytes
                 print('Done')
@@ -72,4 +75,3 @@ def initialize_model():
                 os.remove(output_path)
                 print('Finished inference')
                 ai_integration.send_result(result_data)
-                
